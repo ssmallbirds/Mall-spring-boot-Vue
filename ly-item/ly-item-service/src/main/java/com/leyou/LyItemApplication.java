2ae5357935827ba8ffc6f5294662b631b4b00447
bird <smallbirdwy@163.com>
2019-05-08 16:58:45 +0800

    基本框架搭建完成 网关/注册中心/商品微服务

---
+++ ly-item/ly-item-service/src/main/java/com/leyou/LyItemApplication.java
@@ -0,0 +1,13 @@
+package com.leyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LyItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LyItemApplication.class, args);
+    }
+}
